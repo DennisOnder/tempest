ba8a0f5f6f213daba9653d65fa3972b772029e48
DennisOnder <webdev.dennisonder@gmail.com>
2019-04-23 02:01:21 +0200

    API calling function for testing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import config from "./config/config";
 
+function rng(): number {
+  return Math.floor(Math.random() * 1000);
+}
+
 const app = express();
 
 app.get("/test", (req, res) => {
   res.json({ test: `Hello World.` });
+});
+
+app.get("/number", async (req, res) => {
+  const randomNumber = await rng();
+  res.json({ randomNumber });
 });
 
 app.listen(config.SERVER_PORT, err => {
--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -2,17 +2,37 @@ import { assert } from "chai";
 import config from "../config/config";
 import axios from "axios";
 
-describe("Server Test", () => {
-  it("should return an object", () => {
-    axios
-      .get(`http://localhost:${config.SERVER_PORT}/test`)
-      .then(res => assert.typeOf(res.data, "object"))
-      .catch(err => console.error(err));
+async function callApi(endpoint) {
+  const res = await axios.get(
+    `http://localhost:${config.SERVER_PORT}${endpoint}`
+  );
+  const { data } = await res;
+  return data;
+}
+
+describe("Server Tests", () => {
+  describe("Test Route", () => {
+    it("should return an object", () => {
+      callApi("/test").then(response => {
+        assert.typeOf(response, "object");
+      });
+    });
+    it("should return an object containing a test prop with a value of Hello World.", () => {
+      callApi("/test").then(response => {
+        assert.propertyVal(response, "test", "Hello World.");
+      });
+    });
   });
-  it("should return an object containing a test prop with a value of Hello World.", () => {
-    axios
-      .get(`http://localhost:${config.SERVER_PORT}/test`)
-      .then(res => assert.propertyVal(res.data, "test", "Hello World."))
-      .catch(err => console.error(err));
+  describe("Number Route", () => {
+    it("should return an object", () => {
+      callApi("/number").then(response => {
+        assert.typeOf(response, "object");
+      });
+    });
+    it("should return an object containing a randomNumber prop with a number.", () => {
+      callApi("/number").then(response => {
+        assert.typeOf(response.randomNumber, "number");
+      });
+    });
   });
 });
